bc29f5d755a7103cb4cbcb8fca5589a741cd04be
Camila Suzuki <camila.suzuki.p@gmail.com>
2021-07-29 17:26:27 -0300

    Department registration view and product list view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,13 +10,16 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
@@ -29,4 +32,12 @@ public class Main extends Application {
 	}
 
 }
+	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	public static void main (String[] args) {
+		launch(args);
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,27 +1,52 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
 	@FXML
-	private MenuItem menuItemCompras;
+	private MenuItem menuItemDepartamento;
 	@FXML
-	private MenuItem menuItemRegistro;
+	private MenuItem menuItemProduto;
+	@FXML
+	private MenuItem menuItemCompras;
 	
 	@FXML
-	public void onMenuItemComprasAction() {
-		System.out.println("onMenuItemComprasAction");
+	private MenuItem menuItemListaProdutos;
+	@FXML 
+	private MenuItem menuItemCarrinho;
+	
+	
+	
+	
+	@FXML
+	public void onMenuItemListaProdutosAction() {
+		loadView("/gui/Compras.fxml");
 	}
 	
 	@FXML
-	public void onMenuItemRegistroAction() {
-		System.out.println("onMenuItemregistroAction");
+	public void onMenuItemRegistroDepartamentoAction() {
+		
+		loadView("/gui/Registro.fxml");
+	}
+	
+	@FXML
+	public void onMenuItemRegistroProdutoAction() {
+		System.out.println("onMenuItemRegistroProdutoAction");
 	}
 	
 
@@ -29,6 +54,23 @@ public class MainViewController implements Initializable {
 	public void initialize(URL uri, ResourceBundle rb) {
 	
 		
+	}
+	
+	private void loadView(String absoluteName) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+		try {
+			VBox newVBox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox =(VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception","Error loading view" ,e.getMessage(),AlertType.ERROR);
+		}
 	}
 
 }
